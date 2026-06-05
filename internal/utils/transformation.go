3bca55d590970f1a1741a4c9bd9ea81c1a75b4a8
Fovir <fovir@disroot.org>
2026-06-05 11:36:48 +0800

    fix: validate bits before convertion
    Signed-off-by: Fovir <fovir@disroot.org>

--- internal/utils/transformation.go
+++ internal/utils/transformation.go
@@ -22,6 +22,10 @@ func Bits2Byte(bits []int) ([]byte, error) {
 		return nil, fmt.Errorf("the length of bits is not divided by 8")
 	}
 
+	if err := validateBits(bits); err != nil {
+		return nil, err
+	}
+
 	bytes := make([]byte, len(bits)/8)
 	for i := range bytes {
 		b := 0
@@ -48,4 +52,13 @@ func Bits2Hex(bits []int) (string, error) {
 		return "", fmt.Errorf("bits to hex failed: %w", err)
 	}
 	return hex.EncodeToString(b), nil
+}
+
+func validateBits(bits []int) error {
+	for _, b := range bits {
+		if b != 0 && b != 1 {
+			return fmt.Errorf("invalid bit: %v", b)
+		}
+	}
+	return nil
 }
