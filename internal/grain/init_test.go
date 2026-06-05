83111bc868df9c80b9b7911dae6e9230b7b97c2e
Fovir <fovir@disroot.org>
2026-06-05 21:02:32 +0800

    test: add testing for `g.Init()`
    Signed-off-by: Fovir <fovir@disroot.org>

--- internal/grain/init_test.go
+++ internal/grain/init_test.go
@@ -58,3 +58,56 @@ func TestGrain128AEADV2_initLFSRNFSR(t *testing.T) {
 		})
 	}
 }
+
+func TestGrain128AEADV2_Init(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for receiver constructor.
+		key      string
+		nonce    string
+		wantLFSR string
+		wantNFSR string
+	}{
+		{
+			name:     "Test Vector 1",
+			key:      "0x00000000000000000000000000000000",
+			nonce:    "0x000000000000000000000000",
+			wantLFSR: "8f395a9421b0963364e2ed30679c8ee1",
+			wantNFSR: "81f7e0c655d035823310c278438dbc20",
+		},
+		{
+			name:     "Test Vector 2",
+			key:      "0x000102030405060708090a0b0c0d0e0f",
+			nonce:    "0x000102030405060708090a0b",
+			wantLFSR: "0e1f950d45e05087c4cd63fd00eab310",
+			wantNFSR: "b3c2e1b1eec1f08c2d6eae957f6af9d0",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k, _ := utils.Hex2Byte(tt.key)
+			n, _ := utils.Hex2Byte(tt.nonce)
+			g, err := New(k, n)
+			if err != nil {
+				t.Fatalf("could not construct receiver type: %v", err)
+			}
+			g.Init()
+
+			gotLFSR, err := utils.Bits2Hex(g.LFSR)
+			if err != nil {
+				t.Fatalf("could not convert LFSR to hex: %v", err)
+			}
+			if gotLFSR != tt.wantLFSR {
+				t.Errorf("gotLFSR = %v, want %v", gotLFSR, tt.wantLFSR)
+			}
+
+			gotNFSR, err := utils.Bits2Hex(g.NFSR)
+			if err != nil {
+				t.Fatalf("could not convert NFSR to hex: %v", err)
+			}
+			if gotNFSR != tt.wantNFSR {
+				t.Errorf("gotNFSR = %v, want %v", gotNFSR, tt.wantNFSR)
+			}
+		})
+	}
+}
