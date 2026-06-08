d183acb2d581689a9a49be960e336c5a933f65ad
Fovir <fovir@disroot.org>
2026-06-08 19:11:33 +0800

    feat(frontend): impelement the handler of `Wrap & Save .key File` button
    Signed-off-by: Fovir <fovir@disroot.org>

--- frontend/ts/key-management.ts
+++ frontend/ts/key-management.ts
@@ -15,6 +15,48 @@ function handleGenerateKeyClick() {
 }
 
 export function RegisterGenerateKeyClick() {
+// Handle the click event on `Wrap & Save .key File` button.
+async function handleWrapSaveKeyClick() {
+  // A tool function.
+  const getInputValueById = (id: string) => {
+    return (document.getElementById(id) as HTMLInputElement).value;
+  };
+
+  // Get required DOM values.
+  const key = getInputValueById("key-management-key-input");
+  const password = getInputValueById("key-management-password");
+  const ad = getInputValueById("key-management-ad");
+  const isHex = (
+    document.getElementById(
+      "key-management-input-mode-hex",
+    ) as HTMLInputElement
+  ).checked;
+
+  // Communicate with backend.
+  const resp = await fetch("/api/wrap-key", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ key, password, ad, isHex }),
+  });
+
+  if (!resp.ok) {
+    const err = await resp.json();
+    alert(err.msg);
+    return;
+  }
+
+  // Create blob and download it.
+  const blob = await resp.blob();
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "wrapped.key";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
   const btn = document.getElementById(
     "key-management-generate-key",
   ) as HTMLButtonElement;
