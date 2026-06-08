8a4d234b4edf72adafc9a600c32919518482f98d
Fovir <fovir@disroot.org>
2026-06-08 19:00:12 +0800

    feat: initialize and register handlers
    Signed-off-by: Fovir <fovir@disroot.org>

---
+++ internal/handler/handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Fovir-GitHub/grain-128aeadv2-go/internal/service"
+)
+
+type Handler struct {
+	srv *service.Service
+}
+
+func New(srv *service.Service) *Handler {
+	return &Handler{srv: srv}
+}
+
+func (h *Handler) Register(mux *http.ServeMux) {
+	mux.HandleFunc("/api/wrap-key", h.handleWrapKey)
+}
+
+func writeError(w http.ResponseWriter, status int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]string{"msg": msg}) //nolint
+}
