5ab1c1f8f23fb8260bb40a760ab9475629f0196a
Fovir <fovir@disroot.org>
2026-06-08 17:31:54 +0800

    feat: create `Keys` structure
    Signed-off-by: Fovir <fovir@disroot.org>

--- internal/keys/keys.go
+++ internal/keys/keys.go
@@ -1,5 +1,13 @@
 package keys
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/Fovir-GitHub/grain-128aeadv2-go/internal/utils"
+)
+
 const (
 	SaltSize   = 16
 	NonceSize  = 12
@@ -7,3 +15,19 @@ const (
 	Iterations = 600_000
 	KeySize    = 16
 )
+
+type Keys struct {
+	Salt       []byte `json:"salt"`
+	Iterations int    `json:"iteration"`
+	Nonce      []byte `json:"nonce"`
+	Tag        []byte `json:"tag"`
+
+	// Wrapped stores the key after KDF and AES-128-CCM encryption.
+	Wrapped []byte `json:"wrapped"`
+}
+
+func New() *Keys {
+	return &Keys{
+		Iterations: Iterations,
+	}
+}
