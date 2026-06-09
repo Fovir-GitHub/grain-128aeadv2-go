a8c001e7d21ae044057f075120b574f9c2c573ed
Fovir <fovir@disroot.org>
2026-06-09 19:03:39 +0800

    feat: add `EncryptionResp`
    Signed-off-by: Fovir <fovir@disroot.org>

--- internal/model/encryption.go
+++ internal/model/encryption.go
@@ -6,3 +6,15 @@ type EncryptionRequest struct {
 	Nonce          string `json:"nonce"`
 	Key            string `json:"key"`
 }
+
+type EncryptionResp struct {
+	// Ciphertext is `IV || ciphertext` in hex format.
+	Ciphertext string `json:"output"`
+
+	// States in hex format.
+
+	LoadedLFSR string `json:"loadedLFSR"`
+	LoadedNFSR string `json:"loadedNFSR"`
+	InitLFSR   string `json:"initLFSR"`
+	InitNFSR   string `json:"initNFSR"`
+}
