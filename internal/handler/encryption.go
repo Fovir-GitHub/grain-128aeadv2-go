120aa96454ee022d1dce08bdff70a359999f618c
Fovir <fovir@disroot.org>
2026-06-10 00:52:51 +0800

    feat: add handler of `encryption` service
    Signed-off-by: Fovir <fovir@disroot.org>

---
+++ internal/handler/encryption.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Fovir-GitHub/grain-128aeadv2-go/internal/model"
+)
+
+func (h *Handler) handleEncryption(w http.ResponseWriter, r *http.Request) {
+	var req model.EncryptionRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid request")
+		return
+	}
+
+	resp, err := h.srv.Encryption.Encrypt(&req)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, resp); err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+	}
+}
