e1c1efb9159fe462a3c303dfcd6b7635a12547b3
Fovir <fovir@disroot.org>
2026-06-11 00:06:39 +0800

    feat: add a function factory to output encryption and decryption handler
    Signed-off-by: Fovir <fovir@disroot.org>

--- internal/handler/cipher.go
+++ internal/handler/cipher.go
@@ -7,20 +7,34 @@ import (
 	"github.com/Fovir-GitHub/grain-128aeadv2-go/internal/model"
 )
 
-func (h *Handler) handleEncryption(w http.ResponseWriter, r *http.Request) {
-	var req model.EncryptionRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		writeError(w, http.StatusBadRequest, "invalid request")
-		return
-	}
+func (h *Handler) handleCipher(isEncryption bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var resp any
+		var err error
 
-	resp, err := h.srv.Cipher.Encrypt(&req)
-	if err != nil {
-		writeError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
+		if isEncryption {
+			var req model.EncryptionRequest
+			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+				writeError(w, http.StatusBadRequest, "invalid request")
+				return
+			}
+			resp, err = h.srv.Cipher.Encrypt(&req)
+		} else {
+			var req model.DecryptionRequest
+			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+				writeError(w, http.StatusBadRequest, "invalid request")
+				return
+			}
+			resp, err = h.srv.Cipher.Decrypt(&req)
+		}
 
-	if err := writeJSON(w, http.StatusOK, resp); err != nil {
-		writeError(w, http.StatusInternalServerError, err.Error())
+		if err != nil {
+			writeError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if err := writeJSON(w, http.StatusOK, resp); err != nil {
+			writeError(w, http.StatusInternalServerError, err.Error())
+		}
 	}
 }
