90f83bf46c69fb7cce51a2fbb851d820e4b1857a
Fovir <fovir@disroot.org>
2026-06-11 01:25:58 +0800

    feat(frontend): implement the interaface of unwrapping key
    Signed-off-by: Fovir <fovir@disroot.org>

--- frontend/ts/key-management.ts
+++ frontend/ts/key-management.ts
@@ -1,4 +1,8 @@
-import { DownloadBlobFile, GenerateHex } from "./utils.js";
+import {
+  DownloadBlobFile,
+  GenerateHex,
+  ReadTextFileContent,
+} from "./utils.js";
 
 // Handle the click event on `Generate Key` button.
 function handleGenerateKeyClick() {
@@ -44,6 +48,47 @@ async function handleWrapSaveKeyClick() {
   DownloadBlobFile(blob, "wrapped.key");
 }
 
+async function handleLoadKeyFileClick() {
+  const getInputValueById = (id: string) => {
+    return (document.getElementById(id) as HTMLInputElement).value;
+  };
+
+  const input = document.getElementById(
+    "key-management-load-key-file",
+  ) as HTMLInputElement;
+
+  const b64 = await ReadTextFileContent(input);
+  const passphrase = getInputValueById("key-management-password");
+  const ad = getInputValueById("key-management-ad");
+  if (!passphrase) {
+    return;
+  }
+
+  const resp = await fetch("/api/unwrap-key", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ b64, passphrase, ad }),
+  });
+  if (!resp.ok) {
+    const err = await resp.json();
+    alert(err.msg);
+    return;
+  }
+
+  const content = await resp.json();
+  const keyField = document.getElementById(
+    "key-management-key-input",
+  ) as HTMLInputElement;
+  keyField.value = content.key;
+}
+
+function registerLoadKeyFileChange() {
+  const input = document.getElementById(
+    "key-management-load-key-file",
+  ) as HTMLInputElement;
+  input.addEventListener("change", handleLoadKeyFileClick);
+}
+
 function registerGenerateKeyClick() {
   const btn = document.getElementById(
     "key-management-generate-key",
@@ -62,4 +107,5 @@ function registerWrapSaveKeyClick() {
 export function RegisterKeyManagementEvents() {
   registerGenerateKeyClick();
   registerWrapSaveKeyClick();
+  registerLoadKeyFileChange();
 }
