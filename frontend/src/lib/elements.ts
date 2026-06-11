355a37dc261bedd589b45ebcf6d02ac24afd550e
Fovir <fovir@disroot.org>
2026-06-11 18:17:08 +0800

    feat: register more DOM elements
    Signed-off-by: Fovir <fovir@disroot.org>

--- frontend/src/lib/elements.ts
+++ frontend/src/lib/elements.ts
@@ -2,9 +2,12 @@ export const els = {
   key: getInputElementById("key-management-key-input"),
   password: getInputElementById("key-management-password"),
   ad: getInputElementById("key-management-ad"),
-  adInputMode: getInputElementById("key-management-input-mode-hex"),
+  adHex: getInputElementById("key-management-input-mode-hex"),
   generateKeyButton: getButtonElementById(
     "key-management-generate-key",
+  ),
+  wrapSaveKeyButton: getButtonElementById(
+    "key-management-wrap-save-key",
   ),
 };
 
