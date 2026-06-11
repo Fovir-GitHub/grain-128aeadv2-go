3f502552dcca515172b43d080da5f28d5e81dba3
Fovir <fovir@disroot.org>
2026-06-11 17:24:38 +0800

    refactor: manage elements in `elements.ts`
    Signed-off-by: Fovir <fovir@disroot.org>

---
+++ frontend/src/lib/elements.ts
@@ -0,0 +1,33 @@
+export const els = {
+  key: getInputElementById("key-management-key-input"),
+  password: getInputElementById("key-management-password"),
+  ad: getInputElementById("key-management-ad"),
+  adInputMode: getInputElementById("key-management-input-mode-hex"),
+  generateKeyButton: getButtonElementById(
+    "key-management-generate-key",
+  ),
+};
+
+export function getKey(): string {
+  return els.key.value;
+}
+
+export function setKey(k: string) {
+  els.key.value = k;
+}
+
+export function getPassword(): string {
+  return els.password.value;
+}
+
+export function setPassword(p: string) {
+  els.password.value = p;
+}
+
+function getInputElementById(id: string): HTMLInputElement {
+  return document.getElementById(id) as HTMLInputElement;
+}
+
+function getButtonElementById(id: string): HTMLButtonElement {
+  return document.getElementById(id) as HTMLButtonElement;
+}
