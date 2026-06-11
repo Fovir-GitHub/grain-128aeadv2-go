b0aff91228c0dd396b4a70f921eae46591854dcd
Fovir <fovir@disroot.org>
2026-06-11 18:35:55 +0800

    feat: add `onChange` dom registration
    Signed-off-by: Fovir <fovir@disroot.org>

--- frontend/src/lib/dom.ts
+++ frontend/src/lib/dom.ts
@@ -1,3 +1,7 @@
 export function onClick(el: HTMLElement, handler: () => void) {
   el.addEventListener("click", handler);
 }
+
+export function onChange(el: HTMLInputElement, handler: () => void) {
+  el.addEventListener("change", handler);
+}
